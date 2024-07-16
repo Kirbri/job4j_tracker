0fb36a5b32f6965e4891390bfe129626ecbd9e18
AnnKornilova <kanna1997@mail.ru>
2024-07-16 12:08:30 +0300

    3.2. Удаление моделей из массива. [#310016]

--- src/main/java/ru/job4j/pojo/Store.java
+++ src/main/java/ru/job4j/pojo/Store.java
@@ -2,31 +2,31 @@ package ru.job4j.pojo;
 
 public class Store {
     public static void main(String[] args) {
-//        Product milk = new Product("Milk", 10);
-//        Product bread = new Product("Bread", 4);
-//        Product egg = new Product("Egg", 19);
-//        Product[] products = new Product[3];
-//        products[0] = milk;
-//        products[1] = bread;
-//        products[2] = egg;
-//        for (int index = 0; index < products.length; index++) {
-//            Product product = products[index];
-//            System.out.println(product.getName() + " - " + product.getCount());
-//        }
-//        System.out.println("Replace milk to oil.");
-//        Product oil = new Product("Oil", 11);
-//        products[0] = oil;
-//        for (int index = 0; index < products.length; index++) {
-//            Product product = products[index];
-//            System.out.println(product.getName() + " - " + product.getCount());
-//        }
-//        System.out.println("Shown only product.count > 10");
-//        for (int index = 0; index < products.length; index++) {
-//            Product product = products[index];
-//            if (product.getCount() > 10) {
-//                System.out.println(product.getName() + " - " + product.getCount());
-//            }
-//        }
+        Product milk = new Product("Milk", 10);
+        Product bread = new Product("Bread", 4);
+        Product egg = new Product("Egg", 19);
+        Product[] products = new Product[3];
+        products[0] = milk;
+        products[1] = bread;
+        products[2] = egg;
+        for (int index = 0; index < products.length; index++) {
+            Product product = products[index];
+            System.out.println(product.getName() + " - " + product.getCount());
+        }
+        System.out.println("Replace milk to oil.");
+        Product oil = new Product("Oil", 11);
+        products[0] = oil;
+        for (int index = 0; index < products.length; index++) {
+            Product product = products[index];
+            System.out.println(product.getName() + " - " + product.getCount());
+        }
+        System.out.println("Shown only product.count > 10");
+        for (int index = 0; index < products.length; index++) {
+            Product product = products[index];
+            if (product.getCount() > 10) {
+                System.out.println(product.getName() + " - " + product.getCount());
+            }
+        }
 
         Product[] productsV2 = new Product[5];
         productsV2[0] = new Product("Milk", 10);
