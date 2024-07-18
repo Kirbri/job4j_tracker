5b6cca7ade225965d09f68087b18b31732228a77
AnnKornilova <kanna1997@mail.ru>
2024-07-18 18:17:50 +0300

    1. Что такое полиморфизм. [#3819 #136582]

--- src/main/java/ru/job4j/polymorphism/FileStore.java
+++ src/main/java/ru/job4j/polymorphism/FileStore.java
@@ -10,11 +10,15 @@ public class FileStore implements Store {
     }
 
     public void save(String data) {
-        //Тут реализация хранения данных в файле
+        /*Тут реализация хранения данных в файле
+
+         */
     }
 
     public String read() {
-        // Тут чтение файла
+        /* Тут чтение файла
+
+         */
         return null;
     }
 }
