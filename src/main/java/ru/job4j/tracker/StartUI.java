8dc2cbd56eccacfc19774525b59989598f018367
AnnKornilova <kanna1997@mail.ru>
2024-07-24 11:25:13 +0300

    1. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -36,6 +36,9 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
+        Log4File log = Log4File.getInstance();
+        log.add("add new Item");
+        log.save();
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
         Tracker tracker = new Tracker();
